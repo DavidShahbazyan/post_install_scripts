95bddda37babad61ca1ad4b83135a7398c798228
David Shahbazyan <DavidShahbazyan@users.noreply.github.com>
2024-10-08 17:13:35 +0400

    Minor UI changes

--- main.sh
+++ main.sh
@@ -21,6 +21,13 @@ SCRIPT_VERSION='1.0'
 
 APP_ARGS='-h --help -v --version --only-once --skip-countdown --simulate --verbose --completion-script'
 
+if [ -f "$SCRIPT_PATH/includes/banners.sh" ]; then
+    source "$SCRIPT_PATH/includes/banners.sh"
+else
+    echo "[ERROR] The $SCRIPT_PATH/includes/banners.sh\" file is missing."
+    exit 1
+fi
+
 ######################################################################################################
 
 SIMULATION_MODE=false
@@ -31,17 +38,12 @@ AVAILABLE_MODULES=()
 EXIT_MENU_INDEX=0
 RUN_ALL_MODULES_MENU_INDEX=-1
 
-ALL_BANNERS=(
-    "                                                    ${LIGHT_GREEN}========================                   \n                                              ${LIGHT_GREEN}========================${NC} ${DARK_GREEN}========${NC}                \n                                           ${LIGHT_GREEN}========================${NC} ${DARK_GREEN}===============${NC}             \n                                        ${LIGHT_GREEN}========================${NC} ${DARK_GREEN}=====================${NC}          \n                                      ${LIGHT_GREEN}========================${NC}  ${DARK_GREEN}========================${NC}        \n                                    ${LIGHT_GREEN}========================${NC}     ${DARK_GREEN}========================${NC}      \n                                   ${LIGHT_GREEN}========================${NC}      ${DARK_GREEN}========================${NC}     \n                                  ${LIGHT_GREEN}========================${NC}                                    \n${CYAN}+++++++++${NC}                        ${LIGHT_GREEN}========================${NC}                        ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}             ${LIGHT_GREEN}==============================================${NC}             ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}               ${LIGHT_GREEN}==========================================${NC}               ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                 ${LIGHT_GREEN}======================================${NC}                 ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                   ${LIGHT_GREEN}=================================${NC}                    ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                      ${LIGHT_GREEN}============================${NC}                      ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                        ${LIGHT_GREEN}========================${NC}                        ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                          ${LIGHT_GREEN}====================${NC}                          ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                            ${LIGHT_GREEN}================${NC}                            ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                              ${LIGHT_GREEN}===========${NC}                               ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                                ${LIGHT_GREEN}=======${NC}                                 ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                                  ${LIGHT_GREEN}===${NC}                                   ${CYAN}+++++++++${NC}\n${CYAN}+++++++++${NC}                                                                        ${CYAN}+++++++++${NC}\n${CYAN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}\n${CYAN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}\n${CYAN}++++++++++ ${YELLOW}An automated script that will help to install some usefull packages${NC} ${CYAN}+++++++++++${NC}\n${CYAN}++++++++++ ${YELLOW}AUTHOR: David Shahbazyan${NC} ${CYAN}++++++++++++++++++++++++++++++++++++${NC} ${YELLOW}v.${SCRIPT_VERSION}${NC} ${CYAN}+++++++++++${NC}\n${CYAN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}\n  ${CYAN}++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++${NC}  "
-    "                                          ${LIGHT_GREEN}####        ####${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                          ${LIGHT_GREEN}###+--------+###${NC}                                          \n                                ${LIGHT_GREEN}#############+--------+#############${NC}                                \n                                ${LIGHT_GREEN}############++--------++############${NC}                                \n                                  ${LIGHT_GREEN}#####++------------------++######${NC}                                 \n                                   ${LIGHT_GREEN}######++--------------+++#####${NC}                                   \n                                     ${LIGHT_GREEN}######++-----------++#####${NC}                                     \n                                      ${CYAN}--+${LIGHT_GREEN}####+--------++###${CYAN}+--+${NC}                                     \n                                ${CYAN}#####+----+${LIGHT_GREEN}###++----++###${CYAN}+----+#####${NC}                                \n                            ${CYAN}########+---..--+${LIGHT_GREEN}###++++###${CYAN}+-------+########${NC}                            \n                         ${CYAN}#######+++---.....---+${LIGHT_GREEN}######${CYAN}++----------+++########${NC}                        \n                     ${CYAN}########++----..........---+${LIGHT_GREEN}##${CYAN}++----------------++########${NC}                     \n                  ${CYAN}########++----..............--------------------------++########${NC}                  \n                 ${CYAN}######+-----.........${YELLOW}An automated script that${NC}${CYAN}--------------+#######${NC}                \n               ${CYAN}#######++--..............${YELLOW}will help to install${NC}${CYAN}----------------++#######${NC}               \n             ${CYAN}#####+-++###++-----.......${YELLOW}some usefull packages${NC}${CYAN}-------------++####+-+#####${NC}             \n            ${CYAN}####+-....-+++##++-----..............---------------------++###++---.--+#####${NC}           \n          ${CYAN}#####+-........--++##+++----${YELLOW}AUTHOR: David Shahbazyan${NC}${CYAN}----+++###++---.....--+#####${NC}          \n         ${CYAN}####+-.............--++###++---.......${YELLOW}v.${SCRIPT_VERSION}${NC}${CYAN}-----------++###++----..........--+####${NC}         \n         ${CYAN}#####+--...............--+####++--......----------++####++----...........--++#####${NC}         \n          ${CYAN}#######+--...............--+####++--...-------++####++------.........--++#######${NC}          \n             ${CYAN}#######++-................-+####++------++####++--..........-----++#######${NC}             \n                ${CYAN}#######++--...............-++###++++###++---..........-----++#######${NC}                \n                 ${CYAN}###+-++##++--...............-+######+---...........---+++##++-+###${NC}                 \n                 ${CYAN}###-...--+###++-............-+######+-..........---++###++--..-###${NC}                 \n                 ${CYAN}###-......--+###++--.......-+########+--.....---++###++--.....-###${NC}                 \n                 ${CYAN}###-..........-+####+--..--###-+##+-+##+-----+####+--.........-###${NC}                 \n                 ${CYAN}###-.............-++###+++##+-.-##+.-+###++####+-.............-###${NC}                 \n                 ${CYAN}###-................-++###++-..-##+..-++###++--...............-###${NC}                 \n                 ${CYAN}###-...................----....-##+....----...................-###${NC}                 \n                 ${CYAN}###+--.........................-##+.........................--+###${NC}                 \n                 ${CYAN}######+--......................-##+.....................--++######${NC}                 \n                   ${CYAN}#######++--..................-##+..................--++########${NC}                  \n                      ${CYAN}#######++--...............-##+...............--++#######${NC}                      \n                         ${CYAN}########++-............-##+............--+########${NC}                         \n                             ${CYAN}########+-.........-##+.........-+########${NC}                             \n                                ${CYAN}#######++--.....-##+.....--++#######${NC}                                \n                                   ${CYAN}########+--..-##+..--++#######${NC}                                   \n                                       ${CYAN}#######+++##+++########${NC}                                      \n                                          ${CYAN}################${NC}                                          \n                                             ${CYAN}##########${NC}                                             "
-    " ${CYAN}+---------------------------------------------------------------------+${NC}\n ${CYAN}| ${YELLOW}An automated script that will help to install some usefull packages${CYAN} |${NC}\n ${CYAN}| ${YELLOW}AUTHOR: David Shahbazyan${CYAN}                                      ${YELLOW}v.${SCRIPT_VERSION}${CYAN} |${NC}\n ${CYAN}+---------------------------------------------------------------------+${NC}"
-)
 
 function print_banner {
     echo
-    RANDOM_INDEX=$(( ( RANDOM % ${#ALL_BANNERS[@]} ) ))
+    RANDOM_INDEX=$(( ( RANDOM % ${#AVAILABLE_BANNERS[@]} ) ))
     # echo "Random index: ${RANDOM_INDEX}"
-    echo -e "${ALL_BANNERS[$RANDOM_INDEX]}"
+    echo -e "${AVAILABLE_BANNERS[$RANDOM_INDEX]}"
 }
 
 function print_usage {
@@ -82,12 +84,17 @@ function load_available_modules() {
     IFS=$'\n' AVAILABLE_MODULES=($(sort <<<"${AVAILABLE_MODULES[*]}")); unset IFS
 }
 
-function list_available_modules() {
+function print_menu {
+
+    # echo -e "${CYAN} 🖹 Choose what do you want to do:${NC}"
+
+    echo -e " ${CYAN}🗒 Choose what do you want to do:${NC}"
+
     for i in "${!AVAILABLE_MODULES[@]}"; do
         print_menu_row "$(($i + 1))" "$(bash ${AVAILABLE_MODULES[$i]} --print-name)"
     done
     RUN_ALL_MODULES_MENU_INDEX=$(($i + 2))
-    echo -e "${NC}${PADD_LINE}${CLEAR_REST_OF_LINE}"
+    echo -e " ${NC}$(print_n_times '─' 60)${CLEAR_REST_OF_LINE}"
     print_menu_row "${RUN_ALL_MODULES_MENU_INDEX}" "ALL ABOVE"
     print_menu_row "${EXIT_MENU_INDEX}" "EXIT"
 }
@@ -96,7 +103,7 @@ function invoke_module {
     write_log "Module: ${1}"
     write_log "Params: ${2}"
     bash $1 $2
-    write_log "\n--------------------------------------------------------"
+    write_log "\n────────────────────────────────────────────────────────"
 }
 
 function _start() {
@@ -120,9 +127,7 @@ function _start() {
         echo -e "${YELLOW} ➣ Simulation mode is:${NC} $(if [ $SIMULATION_MODE == true ]; then echo ${DARK_GREEN}'ON (no real changes will be done)'${NC}; else echo ${RED}'OFF (any choice you do will make real changes to your OS)'${NC}; fi)"
 
         echo
-        # echo -e "${CYAN} 🖹 Choose what do you want to do:${NC}"
-        echo -e "${CYAN} 🗒 Choose what do you want to do:${NC}"
-        list_available_modules
+        print_menu
         [ "${invalid_coice_error}" != '' ] && print_error "${invalid_coice_error}"
         echo
 
